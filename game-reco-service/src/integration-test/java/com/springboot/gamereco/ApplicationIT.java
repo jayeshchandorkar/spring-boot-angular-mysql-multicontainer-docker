574befffd6e55293df7b60d786cd3b692a919735
jchandorkar <jchandorkar@gmail.com>
2017-02-24 16:09:28 +0100

    Updated unit tests and integration tests.

--- game-reco-service/src/integration-test/java/com/springboot/gamereco/ApplicationIT.java
+++ game-reco-service/src/integration-test/java/com/springboot/gamereco/ApplicationIT.java
@@ -4,7 +4,16 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.springboot.gamereco.domain.Customer;
+import com.springboot.gamereco.domain.Recommendation;
+import com.springboot.gamereco.repository.CustomerRepository;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -27,15 +37,88 @@ public class ApplicationIT {
     @Autowired
     private TestRestTemplate template;
 
+    @Autowired
+    private CustomerRepository customerRepository;
+
     @Before
     public void setUp() throws Exception {
         this.base = new URL("http://localhost:" + port + "/");
+        Customer customer = new Customer();
+        customer.setCustomerNumber(123);
+        customer.setRecommendationActive(true);
+        customer.setRecommendations(new ArrayList<Recommendation>());
+        Recommendation recommendation = new Recommendation();
+        recommendation.setProductName("Rec 1");
+        recommendation.setCustomer(customer);
+        customer.getRecommendations().add(recommendation);
+        Recommendation recommendationX = new Recommendation();
+        recommendationX.setProductName("Rec 1");
+        recommendationX.setCustomer(customer);
+        customer.getRecommendations().add(recommendationX);
+
+        Customer customer1 = new Customer();
+        customer1.setCustomerNumber(321);
+        customer1.setRecommendationActive(false);
+        Recommendation recommendation1 = new Recommendation();
+        recommendation1.setProductName("Rec 11");
+        recommendation1.setCustomer(customer1);
+        customer1.setRecommendations(Collections.singletonList(recommendation1));
+
+        customerRepository.save(customer);
+        customerRepository.save(customer1);
+    }
+
+    @After
+    public void cleanUp() throws Exception {
+        customerRepository.delete(123);
     }
 
     @Test
-    public void getHello() throws Exception {
-        ResponseEntity<String> response = template.getForEntity(base.toString(),
+    public void customerNotFound() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString() + "customers/98765432/games/recommendations?count=5",
                 String.class);
-        assertThat(response.getBody(), equalTo("Awesome"));
+        // Returns 404 status code if the customer with given customer number does not exist.
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
     }
+
+    @Test
+    public void recommendationInActive() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString() + "customers/321/games/recommendations?count=5",
+                String.class);
+        // Returns 404 status code if the recommendation flag is not set.
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
+    }
+
+    @Test
+    public void getRecommendationForCustomerWithCount() throws Exception {
+
+        ResponseEntity<String> response = template.getForEntity(base.toString() + "customers/123/games/recommendations?count=1",
+                String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(response.getBody());
+        ArrayNode recommendations = (ArrayNode) responseJson;
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat( recommendations.size(), equalTo(1));
+    }
+
+    @Test
+    public void getAllCustomers() throws Exception {
+
+        ResponseEntity<String> response = template.getForEntity(base.toString() + "customers",
+                String.class);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode responseJson = objectMapper.readTree(response.getBody());
+        ArrayNode customers = (ArrayNode) responseJson;
+
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+        assertThat( customers.size(), equalTo(2));
+        assert( customers.get(0).get("customerNumber").canConvertToLong());
+        assertThat( customers.get(0).get("customerNumber").toString(), equalTo("123"));
+    }
+
+
+
 }
--- game-reco-service/src/test/java/com/springboot/gamereco/ApplicationTests.java
+++ game-reco-service/src/test/java/com/springboot/gamereco/ApplicationTests.java
@@ -1,18 +1,29 @@
 package com.springboot.gamereco;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.equalTo;
 
+import com.springboot.gamereco.domain.Customer;
+import com.springboot.gamereco.domain.Recommendation;
+import com.springboot.gamereco.exception.CustomerNotfoundException;
+import com.springboot.gamereco.exception.InActiveRecommendationException;
+import com.springboot.gamereco.service.CustomerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 @RunWith(SpringRunner.class)
@@ -22,10 +33,70 @@ public class ApplicationTests {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    CustomerService customerServiceMock;
+
+    @Test
+    public void customerNotFound() throws Exception {
+        BDDMockito.given(customerServiceMock.getRecommendations(98765432l, 5)).willThrow(new CustomerNotfoundException());
+        mvc.perform(MockMvcRequestBuilders.get("/customers/98765432/games/recommendations?count=5"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void recommendationInActiveForCustomer() throws Exception {
+
+        Customer customer = new Customer();
+        customer.setRecommendationActive(false);
+        customer.setCustomerNumber(12345l);
+
+        BDDMockito.given(customerServiceMock.getRecommendations(98765432l, 5)).willThrow(new InActiveRecommendationException());
+        mvc.perform(MockMvcRequestBuilders.get("/customers/98765432/games/recommendations?count=5"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void getRecommendationForCustomer() throws Exception {
+
+        List<Recommendation> recommendationList = new ArrayList<Recommendation>();
+
+        Customer customer = new Customer();
+        customer.setRecommendationActive(true);
+        customer.setCustomerNumber(98765432l);
+        customer.setRecommendations(recommendationList);
+        Recommendation recommendation = new Recommendation();
+        recommendation.setProductName("Rec 1");
+        recommendation.setId(1);
+        recommendation.setCustomer(customer);
+        recommendationList.add(recommendation);
+
+        Recommendation recommendation1 = new Recommendation();
+        recommendation1.setProductName("Rec 2");
+        recommendation1.setId(2);
+        recommendation1.setCustomer(customer);
+        recommendationList.add(recommendation1);
+
+        BDDMockito.given(customerServiceMock.getRecommendations(98765432l, 1)).willReturn(recommendationList);
+        mvc.perform(MockMvcRequestBuilders.get("/customers/98765432/games/recommendations?count=1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].productName", is(recommendationList.get(0).getProductName())));
+    }
+
     @Test
     public void getText() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/customers").accept(MediaType.APPLICATION_JSON))
+
+        Customer customer = new Customer();
+        customer.setRecommendationActive(false);
+        customer.setCustomerNumber(12345);
+
+        BDDMockito.given(customerServiceMock.getAllCustomers()).willReturn(Collections.singletonList(customer));
+
+        mvc.perform(MockMvcRequestBuilders.get("/customers"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Awesome")));
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].customerNumber", is(new Long(customer.getCustomerNumber()).intValue())))
+                .andExpect(jsonPath("$[0].recommendationActive", is(customer.isRecommendationActive())));
     }
+
 }
